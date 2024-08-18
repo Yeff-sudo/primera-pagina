7c36de2080683489482307187df8ca730054dce6
Yeff-sudo <yefergemas1@gmail.com>
2024-08-18 00:42:50 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const buttonNo = document.querySelector('#no')
+const buttonYes = document.querySelector('#yes')
+
+let fontSize = 2
+
+let messages = [
+  'Estas segura?',
+  'Piensalo bien',
+  'Piensalo muy bien',
+  '¿Mi vida? ¿No?',
+  'Piensalo solo un momento mas',
+  'El otro boton esta mas lindo',
+  'Mira el otro botón'
+]
+
+buttonNo.addEventListener('click', () => {
+  fontSize = fontSize + .5
+  buttonYes.style.fontSize = `${fontSize}rem`
+
+  const indexRandom = Math.floor(Math.random() * messages.length)
+
+  buttonNo.textContent = messages[indexRandom]
+})
+
+buttonYes.addEventListener('click', () => {
+  document.querySelector('#message').style.display = 'flex'
+})
